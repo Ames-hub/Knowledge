827e97af71476f79ccbfb297280cc3e43c5bad84
Amelia <114128556+Ames-hub@users.noreply.github.com>
2025-11-11 16:20:15 +0800

    Update docstring in view function for clarity and consistency

--- modules/signals/route_funcs/example.py
+++ modules/signals/route_funcs/example.py
@@ -6,11 +6,16 @@ def main():
     # Returns 200 as the http code. Remove the second item (The 200) to use the default http code set in the route.
     return "Hello from the example route function!", 200
 
+# Program calls "view" function to get data to show on the route.
 def view():
     """
     This function is called to get whatever data is assosciated with the function that this route gathered.
     Eg, a route was called and set "your_key" to "your_value", then this function COULD return "your_value".
 
-    So, "main" gathers data -> "view" returns data
+    So, "main" gathers data -> "view" returns data.
+    
+    Eg, for usage tracking,
+    main() increments a counter each time the route is called,
+    view() returns the current value of that counter.
     """ 
     return "This is the view function response.", 200
